b6533cdb8fe724c218e2ba92bcb2126a28707c02
DavePearce <david.pearce@ecs.vuw.ac.nz>
2020-04-16 22:46:20 +1200

    Add native methods for DOM
    This adds missing native methods (e.g. for setTimeout).

--- src/js/w3c/dom.js
+++ src/js/w3c/dom.js
@@ -1,3 +1,35 @@
 function w3c$dom$alert(message) {
     alert(message);
 }
+
+function w3c$dom$setTimeout(callback,ms){
+    setTimeout(callback,ms);
+}
+
+function w3c$dom$setInterval(callback, ms) {
+    setInterval(callback,ms);
+}
+
+function w3c$dom$clearTimeout() {
+    clearTimeout();
+}
+
+function w3c$dom$clearInterval() {
+    clearInterval();
+}
+
+function w3c$dom$open(URL, name, specs, replace) {
+    open(URL,name,specs,replace)
+}
+
+function w3c$dom$prompt(text, defaultText) {
+    prompt(text,defaultText);
+}
+
+function w3c$dom$confirm(text) {
+    confirm(text);
+}
+
+function w3c$dom$close() {
+    close();
+}
